f1e415e9684cdf2140cea575d8d732b2788f1f09
emanero francisco <emanero2000@gmail.com>
2025-12-19 19:46:55 +0000

    quiero el confetti no sea infinito

--- src/components/Confetti.tsx
+++ src/components/Confetti.tsx
@@ -9,17 +9,24 @@ export function Confetti() {
   useEffect(() => {
     if (isFired.current) return;
     isFired.current = true;
-    
+
+    const duration = 3 * 1000; // 3 seconds
+    const animationEnd = Date.now() + duration;
     const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+    const colors = ['#fde68a', '#d8b4fe', '#a78bfa', '#60a5fa', '#facc15'];
 
     function randomInRange(min: number, max: number) {
       return Math.random() * (max - min) + min;
     }
 
     const interval = window.setInterval(() => {
-      const particleCount = 50;
-      // New color palette
-      const colors = ['#fde68a', '#d8b4fe', '#a78bfa', '#60a5fa', '#facc15'];
+      const timeLeft = animationEnd - Date.now();
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+
+      const particleCount = 50 * (timeLeft / duration);
       confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 }, colors });
       confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 }, colors });
     }, 250);
